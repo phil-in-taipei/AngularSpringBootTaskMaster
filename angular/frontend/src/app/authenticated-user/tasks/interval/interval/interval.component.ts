5682d9246627fbdd535ab6887ee76b60743316a5
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-03-19 17:43:45 +0800

    Additional files in tree for monthly and interval task group modules for lazy/loading bundles/routes -- Angular/frontend

---
+++ angular/frontend/src/app/authenticated-user/tasks/interval/interval/interval.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interval',
+  standalone: true,
+  imports: [],
+  templateUrl: './interval.component.html',
+  styleUrl: './interval.component.css'
+})
+export class IntervalComponent {
+
+}
