3c428fa5867c49eadf0e559291e8692a94245f32
phil-in-taipei <sweeneyphilip11@gmail.com>
2024-03-25 20:08:45 +0800

    List of single tasks scheduled for the day shown in template of landing page with bootstrap button to toggle display -- Angular/frontend

---
+++ angular/frontend/src/app/authenticated-user/landing/individual-task/individual-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-individual-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './individual-task.component.html',
+  styleUrl: './individual-task.component.css'
+})
+export class IndividualTaskComponent {
+
+}
---
+++ angular/frontend/src/app/authenticated-user/tasks/single/list/single-task/single-task.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-task.component.html',
+  styleUrl: './single-task.component.css'
+})
+export class SingleTaskComponent {
+
+}
